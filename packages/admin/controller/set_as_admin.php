feebbf318a7d7afe089cd308ae9dc53fd1f26cca
rafa <rrodriguezramirez@gmail.com>
2014-04-13 22:35:14 -0400

    hackaton: sync work

--- packages/admin/controller/set_as_admin.php
+++ packages/admin/controller/set_as_admin.php
@@ -1,14 +1,15 @@
 <?php
-
 include_once framework::resolve('packages/base/model/Security.php');
 
-if (!Security::isSessionStartedByAdmin())
-    framework::redirect("admin/home");
+if (! Security::isSessionStartedByAdmin())
+	framework::redirect("admin/home");
 
 $email = $_GET['email'];
 $user = Security::getUser($email);
 $user['ADMINISTRATOR'] = 1;
-unset($user['PASSWORD']);
-Security::updateUserInformation($email, $user);
+
+Security::updateUserInformation($email, array(
+		'ADMINISTRATOR' => 1
+));
 
 framework::redirect("admin/users");
--- packages/admin/controller/set_as_user.php
+++ packages/admin/controller/set_as_user.php
@@ -1,15 +1,15 @@
 <?php
-
 include_once framework::resolve('packages/base/model/Security.php');
 
-if (!Security::isSessionStartedByAdmin())
-    framework::redirect("admin/home");
+if (! Security::isSessionStartedByAdmin())
+	framework::redirect("admin/home");
 
 $user = Security::getUser($_GET['email']);
 
 $user['ADMINISTRATOR'] = 0;
-unset($user['PASSWORD']);
 
-Security::updateUserInformation($_GET['email'], $user);
+Security::updateUserInformation($_GET['email'], array(
+		'ADMINISTRATOR' => 0
+));
 
 framework::redirect("admin/users");
