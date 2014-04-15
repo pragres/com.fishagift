d87c9984f480a844ec8f2604e844646ad96fbf96
Salvi Pascual <salvi.pascual@gmail.com>
2014-04-14 22:34:45 -0400

    Fix typos on 404

--- packages/base/i18n/es/error404.php
+++ packages/base/i18n/es/error404.php
@@ -1,8 +1,8 @@
 <?php
-	$i18n['explanation'] = "No encontramos la página que usted busca :'-(";
+	$i18n['explanation'] = "No encontramos la p&aacute;gina que usted busca :'-(";
 	$i18n['introduction'] = "Que triste que lea este mensaje,<br/>fue escrito para nunca ser visto.";
-	$i18n['badNews'] = "Usted llegó a esta página porque escribió una URL incorrecta en su navegador o porque una sección de nuestro sitio fue eliminado o está temporalmente caido. Sabemos que esta esta aquí, al menos vamos a ver que paso y arreglar el error para nuestro próximo usuario.";
-	$i18n['goodNews'] = 'La buena noticia es que usted no perdió nada de lo que estaba haciendo. Presione el botón "Volver atrás" y verá la página de la cual vino. Si su intención es mandar un nuevo regalo, solo presione el botón verde :-)';
-	$i18n['btnGoBack'] = "Volver atrás";
+	$i18n['badNews'] = "Usted lleg&oacute; a esta p&aacute;gina porque escribi&oacute; una URL incorrecta en su navegador o porque una secci&oacute;n de nuestro sitio fue eliminado o est&aacute; temporalmente caido. Sabemos que esta esta aqu&iacute;, al menos vamos a ver que paso y arreglar el error para nuestro pr&oacute;ximo usuario.";
+	$i18n['goodNews'] = 'La buena noticia es que usted no perdi&oacute; nada de lo que estaba haciendo. Presione el bot&oacute;n "Volver atr&aacute;s" y ver&aacute; la p&aacute;gina de la cual vino. Si su intenci&oacute;n es mandar un nuevo regalo, solo presione el bot&oacute;n verde :-)';
+	$i18n['btnGoBack'] = "Volver atr&aacute;s";
 	$i18n['btnNewPresent'] = "Comenzar un nuevo regalo";
 ?>
