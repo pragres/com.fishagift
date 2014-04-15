658da9cc73b6729b310afe0359debfd03fdc0cb7
rafa <rrodriguezramirez@gmail.com>
2014-04-15 00:24:36 -0400

    hackaton: bugfix login

--- orm/Trace.php
+++ orm/Trace.php
@@ -29,7 +29,7 @@ class Trace {
 		$pkg = framework::getValue('package');
 		$pag = framework::getValue('page');
 		
-		if (empty($pkg) || empty($pag) || is_null($pkg) || is_null($pkg) || "$pkg" == "" || "$pkg" == "")
+		if (empty($pkg) || empty($pag) || is_null($pkg) || is_null($pag) || "$pkg" == "" || "$pag" == "")
 			$this->location = null;
 		else
 			$this->location = $pkg . '/' . $pag;
